724bfcbdd4f65ab8af1c7bd5eccfbe393a7df063
João Marcos <marcospb19@hotmail.com>
2018-11-11 02:21:12 -0300

    main.c e libraries criados main.c tem a função main, que recebe diretamente a input do usuário. A pasta Libraries foi criada para armazenar todos os arquivos .c e .h fora o main Adicionados colors.c e .h que colorem o terminal e messages .c e .h que imprimem as mensagens coloridas de saída.
    	New:		libraries/colors.c
    			libraries/colors.h
    			libraries/messages.c
    			libraries/messages.h
    
    	Fixed:		.gitignore

---
+++ libraries/messages.h
@@ -0,0 +1,21 @@
+#include "messages.c"
+
+// messages.c contain functions to print messages in the terminal
+
+void welcomeMessage();
+// Print welcome message, with 'help', 'credits' and 'license' suggestions
+
+void helpMessage();
+// Lists all the commands currently available
+
+void creditsMessage();
+// List the creators and official online repository
+
+void licenseMessage();
+// Explain in short the license, and print link to the full version
+
+void finishMessage();
+// Message printed when the user finished the application correctly
+
+void noInputMessage();
+//Prints if input is invalid
