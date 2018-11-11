515f879e6f8654bf8d28501bbbf522f12696c5eb
João Marcos <marcospb19@hotmail.com>
2018-11-11 02:44:40 -0300

    Corrigindo conflito
    Depois desse commit eu vou parar de me importar com a organização do log
    Quanto à isso, o git é muito inflexível

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include "libraries/messages.h"
+
+int main()
+{
+	welcomeMessage();
+	char userInput[50];
+
+	while (true)
+	{
+		printf("»»» ");
+		scanf(" %s" , userInput);
+
+		if (strcmp("help" , userInput) == 0)
+			helpMessage();
+		else if (strcmp("license" , userInput) == 0)
+			licenseMessage();
+		else if (strcmp("credits" , userInput) == 0)
+			creditsMessage();
+
+		// Quit
+		else if (strcmp("exit" , userInput) == 0 || userInput[0] == 'q')
+		{
+			finishMessage();
+			break;
+		}
+		else
+			noInputMessage();
+	}
+}
