337adc82c339a4e749c8d99e096f10e8550afb98
João Marcos <marcospb19@hotmail.com>
2018-11-12 15:40:19 -0300

    Checkpoint 1 e 2
    Criadas as funções createTable, listTables e strcommand em tables.c
    createTable cria uma tabela com nome especificado pelo usuário, e preenche as
    colunas, listTables lista todas as tabelas em listOfTables.txt e strcommand é
    uma função para indentificar comandos no início de strings, caso posteriormente
    as funções possam receber mais argumentos na mesma linha.

---
+++ libraries/tables.h
@@ -0,0 +1,14 @@
+#ifndef TABLES_H
+#define TABLES_H
+#include "tables.c"
+#endif
+
+int strcommand(char string[] , char command[]);
+// Similar to strcmp, returns 1 if command is inside of the string (instead of returning 0, like strcmp)
+
+int createTable(char nameOfTable[]);
+// Creates tables that can store integers, decimal numbers, characters and strings
+// First adds the table to the listOfTables.txt, and then creates the table
+
+int listTables();
+// List all the tables inside of listOfTables.txt
