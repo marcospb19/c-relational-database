3f05781093c845409c78d7dfb856ee659dacb6f7
marcospb19 <marcospb19@hotmail.com>
2018-12-02 16:02:36 -0300

    make.sh: added time output and removed clear

--- make.sh
+++ make.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-clear && clear
-gcc -Werror -Wall libraries/struct.c libraries/command.c libraries/list.c libraries/colors.c libraries/messages.c libraries/tables.c main.c -o sgbd && echo "Build successful."
+\time -f " Time: %es" gcc -Werror -Wall libraries/struct.c libraries/command.c libraries/list.c libraries/colors.c libraries/messages.c libraries/tables.c main.c -o sgbd && echo " Build successful"
